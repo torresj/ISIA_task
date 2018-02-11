2cb30364930d48a7c11ee027e0d5255020c2fd97
Miguel Eduardo Molina Chaparro <Miguel@MacBook-Pro-de-Miguel.local>
2018-02-11 12:46:54 +0100

    Invertir matriz

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -33,7 +33,87 @@ public class Matriz {
     public Dimension getDimension(){
         return new Dimension(datos.length, datos[0].length);
     }
-    
+public double[][] matrixInversa(double[][] matriz) {
+    double det=1/determinante(matriz);
+    double[][] nmatriz=matrixAdjunta(matriz);
+    multiplicarMatrix(det,nmatriz);
+    return nmatriz;
+}
+ 
+public void multiplicarMatrix(double n, double[][] matriz) {
+    for(int i=0;i<matriz.length;i++)
+        for(int j=0;j<matriz.length;j++)
+            matriz[i][j]*=n;
+}
+ 
+public static double[][] matrixAdjunta(double [][] matriz){
+    return matrixTranspuesta(matrixCofactores(matriz));
+}
+ 
+public static double[][] matrixCofactores(double[][] matriz){
+    double[][] nm=new double[matriz.length][matriz.length];
+    for(int i=0;i<matriz.length;i++) {
+        for(int j=0;j<matriz.length;j++) {
+            double[][] det=new double[matriz.length-1][matriz.length-1];
+            double detValor;
+            for(int k=0;k<matriz.length;k++) {
+                if(k!=i) {
+                    for(int l=0;l<matriz.length;l++) {
+                        if(l!=j) {
+                        int indice1=k<i ? k : k-1 ;
+                        int indice2=l<j ? l : l-1 ;
+                        det[indice1][indice2]=matriz[k][l];
+                        }
+                    }
+                }
+            }
+            detValor=determinante(det);
+            nm[i][j]=detValor * (double)Math.pow(-1, i+j+2);
+        }
+    }
+    return nm;
+}
+ 
+public static double[][] matrixTranspuesta(double [][] matriz){
+    double[][]nuevam=new double[matriz[0].length][matriz.length];
+    for(int i=0; i<matriz.length; i++)
+    {
+        for(int j=0; j<matriz.length; j++)
+            nuevam[i][j]=matriz[j][i];
+    }
+    return nuevam;
+}
+ 
+public static double determinante(double[][] matriz)
+{
+    double det;
+    if(matriz.length==2)
+    {
+        det=(matriz[0][0]*matriz[1][1])-(matriz[1][0]*matriz[0][1]);
+        return det;
+    }
+    double suma=0;
+    for(int i=0; i<matriz.length; i++){
+    double[][] nm=new double[matriz.length-1][matriz.length-1];
+        for(int j=0; j<matriz.length; j++){
+            if(j!=i){
+                for(int k=1; k<matriz.length; k++){
+                int indice=-1;
+                if(j<i)
+                indice=j;
+                else if(j>i)
+                indice=j-1;
+                nm[indice][k-1]=matriz[j][k];
+                }
+            }
+        }
+        if(i%2==0)
+        suma+=matriz[i][0] * determinante(nm);
+        else
+        suma-=matriz[i][0] * determinante(nm);
+    }
+    return suma;
+}
     public static Matriz sumarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles { 
         if(! a.getDimension().equals(b.getDimension())) throw new DimensionesIncompatibles("La suma de matrices requiere matrices de las mismas dimensiones");        
         int i, j, filasA, columnasA; 
